54aefda04e7252e386b02615534ed7f1b1061995
Elvis Finol <e.finol7@gmail.com>
2021-10-03 01:54:55 +0200

    second commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Welcome to this place where I share a little about who I am and what I am learning to become a better engineer and growing my career in tech.</p>
-        <p>I love computer science and tech as it is always exciting and fun. I have been working as System-Engineer on several companies like Teradata and Thomson Reuters, exposed to a variety of environments and a diversity of challenges.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
