0f2247d62834c4045fb5d2add086ad26ddf8f71b
Elvis Finol <e.finol7@gmail.com>
2021-10-03 11:19:57 +0200

    third commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Welcome to this place where I share a little about who I am and what I am learning to become a better engineer and growing my career in tech.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
